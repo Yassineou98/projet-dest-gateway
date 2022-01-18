75e3507c9501dd7e7939cb5705a98397c99099e0
Ayoub <ayoubzoubeidi@gmail.com>
2022-01-18 10:36:30 +0100

    Initial commit.

---
+++ src/main/java/com/ing/nzy/gateway/ProjetDestGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ing.nzy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDestGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDestGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ing/nzy/gateway/ProjetDestGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.nzy.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDestGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
